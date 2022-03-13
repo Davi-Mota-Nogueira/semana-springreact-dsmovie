b287574448113993623e28cd45029c8af509a066
Davi-Mota-Nogueira <davidamota.nogueira@gmail.com>
2022-03-13 20:16:13 -0300

    Achar filmes por página e por ID

---
+++ backend/src/main/java/com/davidevelopment/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.davidevelopment.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davidevelopment.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie,Long>{
+
+}
